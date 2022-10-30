6d88ad38247a706d7bbd51f5f8028fd789322e6b
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-10-30 18:15:01 +0200

    Alignes Delete Btn in row

--- src/components/contactList/ContactList.styled.jsx
+++ src/components/contactList/ContactList.styled.jsx
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 export const List = styled.ul``;
 
 export const ListItem = styled.li`
+  display: flex;
+  width: 350px;
+  justify-content: space-between;
   font-size: 20px;
   list-style: disc;
-  margin-left: 20px;
 
   &:not(:last-child) {
     margin-bottom: 5px;
